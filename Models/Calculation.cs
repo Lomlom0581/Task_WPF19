474eaaf88639d6a74d94a3fa2d302aa3b6c26188
Антон Еремеев <kzanton_81@mail.ru>
2022-02-14 23:15:14 +0300

    Добавьте файлы проекта.

---
+++ Models/Calculation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_WPF19.Models
+{
+    static class Calculation
+    {
+        public static double Calculate(double R)
+        {
+            return 2 * Math.PI * R;
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Task_WPF19.Models;
+
+namespace Task_WPF19.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnProprtyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnProprtyChanged();
+            }
+        }
+
+        private double cicleLenth;
+        public double CicleLength
+        {
+            get => cicleLenth;
+            set
+            {
+                cicleLenth = value;
+                OnProprtyChanged();
+            }
+        }
+
+        public ICommand CicleCommand { get; }
+        private void OnCicleCommandExecute(object p)
+        {
+            CicleLength = Calculation.Calculate(Radius);
+        }
+
+        private bool CanCicleCommandExecute(object p)
+        {
+            return Radius >= 0;
+        }
+
+        public MainWindowViewModel()
+        {
+            CicleCommand = new RelayCommand(OnCicleCommandExecute, CanCicleCommandExecute);
+        }
+    }
+}
